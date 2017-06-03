edfdf2b079883d956135dd85077d4a31e9a14017
Collin Moore <collin.moore96@gmail.com>
2017-06-03 12:30:42 -0600

    Added rose favicon; Added space to tab name; Added styling to the #box; Added all the buttons for the favorite color and the ability to change box

--- FavoriteThings/src/app/app.component.ts
+++ FavoriteThings/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'Favorite Things';
+  title = 'Favorite Things';
+  favoriteColor = 'gray';
+
+  setColor(selectedColor: string): void {
+    this.favoriteColor = selectedColor;
+  }
+
+  updateColor(): void {
+    console.log('You selected update');
+  }
 }
+
