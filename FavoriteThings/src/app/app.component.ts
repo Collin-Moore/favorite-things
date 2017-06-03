5599e58a22dd8a3ffb5f4e3b70f034444391ea0b
Collin Moore <collin.moore96@gmail.com>
2017-06-03 12:47:25 -0600

    Local only, my version of Favorite Number, going to adopt presented solution though. Won't need conditionals then.

--- FavoriteThings/src/app/app.component.ts
+++ FavoriteThings/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Favorite Things';
   favoriteColor = 'gray';
+  favoriteNumber = 0;
 
   setColor(selectedColor: string): void {
     this.favoriteColor = selectedColor;
@@ -15,6 +16,16 @@ export class AppComponent {
 
   updateColor(): void {
     console.log('You selected update');
+  }
+
+  setNumber(action: string): void {
+    if (action === '+') {
+      this.favoriteNumber += 1;
+    } else if (action === '-') {
+      this.favoriteNumber -= 1;
+    } else if (action === '0') {
+      this.favoriteNumber = 0;
+    }
   }
 }
 
